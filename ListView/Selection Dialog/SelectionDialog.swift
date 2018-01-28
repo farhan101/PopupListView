79492040af8a1d503bef286fa4fbc1a158c221a2
Farhan Arshad <mindmanager@gmail.com>
2018-01-28 14:17:42 +0500

    -

---
+++ ListView/Selection Dialog/SelectionDialog.swift
@@ -0,0 +1,150 @@
+//
+//  SelectionDialog.swift
+//  tossdown
+//
+//  Created by MacMini on 10/19/17.
+//  Copyright © 2017 tossdown. All rights reserved.
+//
+
+import UIKit
+
+
+//Help: https://stackoverflow.com/questions/11236367/display-clearcolor-uiviewcontroller-over-uiviewcontroller
+
+struct SelectionDialogStruct{
+
+    var id : String?
+    var title: String?
+    var description: String?
+    
+}
+struct SelectionDialogFields{
+    
+    static let id = "id"
+    static let title = "title"
+    static let description = "description"
+    
+}
+
+class SelectionDialog: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+
+    @IBOutlet weak var mBase: UIView!
+    @IBOutlet weak var mCrossBack: UIView!
+    @IBOutlet weak var mTable: UITableView!
+    @IBOutlet weak var mtxtSearch: UITextField!
+    @IBOutlet weak var mlblDialogTitle: UILabel!
+    
+    //fileprivate var mData : NSArray?
+    fileprivate var mData : [SelectionDialogStruct]?
+    
+    var delegate: SelectionDialogProtocol!
+    
+    var data : [SelectionDialogStruct]{
+    
+        set{
+            mData = newValue
+        }
+        get{
+            return mData!
+        }
+    }
+    var dialogTitle: String?{
+        willSet{}
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        //self.view.backgroundColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0)
+        mBase.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
+        let tap = UITapGestureRecognizer(target: self, action: #selector(actionCross(_ :)))
+        mCrossBack.addGestureRecognizer(tap)
+        mTable.dataSource = self
+        mTable.delegate = self
+        mtxtSearch.delegate = self
+        mtxtSearch.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
+        
+        if (dialogTitle != nil) {
+            mlblDialogTitle.text = dialogTitle
+        }
+        
+    }
+
+    @objc func textFieldDidChange(textField: UITextField){
+        
+        mTable.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        if mtxtSearch.text! == ""{
+            
+            return 60.0
+        }
+        
+        var height: CGFloat = 0.0
+        
+        let data: SelectionDialogStruct = mData![indexPath.row]
+        let title = data.title
+        
+        if title?.lowercased().range(of: mtxtSearch.text!.lowercased()) != nil{
+            
+            height = 60.0
+            
+        }
+        
+        
+        return height
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mData?.count ?? 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        if tableView.rectForRow(at: indexPath).height == 0.0 {
+            return UITableViewCell()
+        }
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "Cell")
+        cell.selectionStyle = .none
+        
+        let data: SelectionDialogStruct = mData![indexPath.row]
+        cell.textLabel?.text = data.title
+        cell.textLabel?.textColor = UIColor.darkGray
+        cell.detailTextLabel?.text = data.description
+        cell.detailTextLabel?.textColor = UIColor.lightGray
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        DispatchQueue.main.async {
+            let data: SelectionDialogStruct = self.mData![indexPath.row]
+            self.dismiss(animated: true) {
+                self.delegate.selectionDialogProtocol(selection: data)
+            }
+        }
+        
+        
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func actionCross(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+protocol SelectionDialogProtocol {
+    func selectionDialogProtocol(selection: SelectionDialogStruct)
+}
--- ListView/ViewController.swift
+++ ListView/ViewController.swift
@@ -8,13 +8,81 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, SelectionDialogProtocol {
+    
+    @IBOutlet weak var txt: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        txt.delegate = self
     }
 
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        readFile()
+        return false
+    }
+    func readFile(){
+        // get the documents folder url
+        if let path = Bundle.main.path(forResource: "data", ofType: "json") {
+            
+            // use path
+            do {
+                let url = URL(fileURLWithPath: path)
+                let mytext = try String(contentsOf: url)
+                //print(mytext)   // "some text\n"
+                
+                if let data = mytext.data(using: .utf8){
+                    
+                    //print(data)
+                    do{
+                        let info = try JSONSerialization.jsonObject(with: data, options: []) as! NSArray
+//                        let flat = info.flatMap({ (obj) -> String in
+//                            let arr = obj as! NSDictionary
+//                            //print(arr)
+//                            //print(arr.value(forKey: "value") as! String)
+//                            return (arr.value(forKey: "value") as! String)
+//                        })
+//                        let flat = info.flatMap({ (obj) -> Any in
+//                            let arr = obj as! NSDictionary
+//                            //print(arr)
+//                            //print(arr.value(forKey: "value") as! String)
+//                            return arr
+//                        })
+//                        let flat = info.map({ (obj) -> String in
+//                            let d = obj as! NSDictionary
+//                            return d.value(forKey: "value") as! String
+//                        })
+//                        print(flat)
+                        let data = info.map({ (obj) -> SelectionDialogStruct in
+                            let d = obj as! NSDictionary
+                            var data = SelectionDialogStruct()
+                            data.id = (d.value(forKey: "id") as! String)
+                            data.title = (d.value(forKey: "value") as! String)
+                            data.description = (d.value(forKey: "details") as! String)
+                            return data
+                        })
+                        let dialog = SelectionDialog()
+                        dialog.data = data
+                        dialog.delegate = self
+                        dialog.dialogTitle = "Select Dialog"
+                        dialog.modalTransitionStyle = .crossDissolve
+                        self.present(dialog, animated: true, completion: nil)
+                    }catch{
+                        print(error.localizedDescription)
+                    }
+                }
+                
+            } catch {
+                print("error loading contents of:", path, error)
+            }
+        }
+        
+        
+    }
+    func selectionDialogProtocol(selection: SelectionDialogStruct) {
+        txt.text = selection.title
+    }
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
