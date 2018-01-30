a3555b3a9106f3ff91280bc830be2fee581ac619
Farhan Arshad <mindmanager@gmail.com>
2018-01-30 14:03:36 +0500

    Country List added.

--- ListView/ViewController.swift
+++ ListView/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Farhan Arshad on 1/26/18.
 //  Copyright © 2018 Farhan Arshad. All rights reserved.
 //
+//Country List Taken From: https://paulund.co.uk/list-countries-json
 
 import UIKit
 
@@ -22,43 +23,19 @@ class ViewController: UIViewController, UITextFieldDelegate, SelectionDialogProt
         return false
     }
     func readFile(){
-        // get the documents folder url
-        if let path = Bundle.main.path(forResource: "data", ofType: "json") {
-            
-            // use path
+        if let path = Bundle.main.path(forResource: "Countries", ofType: "json") {
             do {
                 let url = URL(fileURLWithPath: path)
                 let mytext = try String(contentsOf: url)
-                //print(mytext)   // "some text\n"
-                
                 if let data = mytext.data(using: .utf8){
-                    
-                    //print(data)
                     do{
                         let info = try JSONSerialization.jsonObject(with: data, options: []) as! NSArray
-//                        let flat = info.flatMap({ (obj) -> String in
-//                            let arr = obj as! NSDictionary
-//                            //print(arr)
-//                            //print(arr.value(forKey: "value") as! String)
-//                            return (arr.value(forKey: "value") as! String)
-//                        })
-//                        let flat = info.flatMap({ (obj) -> Any in
-//                            let arr = obj as! NSDictionary
-//                            //print(arr)
-//                            //print(arr.value(forKey: "value") as! String)
-//                            return arr
-//                        })
-//                        let flat = info.map({ (obj) -> String in
-//                            let d = obj as! NSDictionary
-//                            return d.value(forKey: "value") as! String
-//                        })
-//                        print(flat)
                         let data = info.map({ (obj) -> SelectionDialogStruct in
                             let d = obj as! NSDictionary
                             var data = SelectionDialogStruct()
-                            data.id = (d.value(forKey: "id") as! String)
-                            data.title = (d.value(forKey: "value") as! String)
-                            data.description = (d.value(forKey: "details") as! String)
+                            data.id = (d.value(forKey: "code") as! String)
+                            data.title = (d.value(forKey: "name") as! String)
+                            data.description = (d.value(forKey: "code") as! String)
                             return data
                         })
                         let dialog = SelectionDialog()
@@ -66,6 +43,7 @@ class ViewController: UIViewController, UITextFieldDelegate, SelectionDialogProt
                         dialog.delegate = self
                         dialog.dialogTitle = "Select Dialog"
                         dialog.modalTransitionStyle = .crossDissolve
+                        dialog.modalPresentationStyle = .overCurrentContext
                         self.present(dialog, animated: true, completion: nil)
                     }catch{
                         print(error.localizedDescription)
